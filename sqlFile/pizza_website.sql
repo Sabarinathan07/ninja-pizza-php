4cc559489a32f15fca3d1cf92620512abc04e539
Sabari <r.sabarinathan22@gmail.com>
2021-06-25 19:00:56 +0530

    sql file added

---
+++ sqlFile/pizza_website.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 22, 2021 at 11:12 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pizza_website`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pizzas`
+--
+
+CREATE TABLE `pizzas` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `ingredients` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pizzas`
+--
+
+INSERT INTO `pizzas` (`id`, `title`, `ingredients`, `email`, `created_at`) VALUES
+(1, 'Pizza title', 'tomato, potato, french fries', 'pizza@gmail.com', '2021-06-22 08:46:10'),
+(3, 'Extra cheese', 'Cheese, Potato, Onion', 'rs.sabarinathan22@gmail.com', '2021-06-22 08:54:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pizzas`
+--
+ALTER TABLE `pizzas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pizzas`
+--
+ALTER TABLE `pizzas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
